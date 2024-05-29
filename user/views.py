d265699c5c24467d456ad6c4c32c399404aac578
Fabricio <fabriciochba@gmail.com>
2024-05-29 12:51:08 -0500

    user fixing process

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveAPIView
+from rest_framework import status, viewsets
+
+from .models import User
+from .serializer import UserSerializer
+
 
 # Create your views here.
+
+
+class UserView(viewsets.ModelViewSet):
+    model = User
+    serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            instance = request.user
+            serializer = UserSerializer(instance)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
