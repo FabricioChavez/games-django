e7799c3a5039059e07f04f9473fc575a8605d663
Fabricio <fabriciochba@gmail.com>
2024-06-03 13:39:27 -0500

    games

--- games/models.py
+++ games/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    id = models.IntegerField(primary_key=True , auto_created=True)
+
+    def __str__(self):
+        return str(self.id)
