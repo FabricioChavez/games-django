da405a7535aaabc8588fa6239f1d8a2d0929db8e
Benjamin Soto <benjamin.soto@utec.edu.pe>
2024-06-04 17:57:14 -0500

    new login and register

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import UserView , login , ProfileViewSet
+from .views import UserView , login , ProfileViewSet , register
 
 router = DefaultRouter()
 router.register(r'users', UserView, basename='user')
@@ -11,4 +11,5 @@ router.register(r'profile', ProfileViewSet, basename='profile')
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -10,6 +10,7 @@ from .models import User
 from .serializers import UserSerializer
 import logging
 from django.shortcuts import get_object_or_404
+from django.db.utils import IntegrityError
 
 logger = logging.getLogger(__name__)
 
@@ -29,17 +30,48 @@ class UserView(viewsets.ModelViewSet):
         return Response({'token': token.key}, status=status.HTTP_201_CREATED, headers=headers)
 
 
+
 @api_view(['POST'])
 @permission_classes([permissions.AllowAny])
-@authentication_classes([])
-def login(request, format=None):
+def login(request):
+    errors = {}
+
+    if 'username' not in request.data:
+        errors['username'] = ["Username is required."]
+    if 'password' not in request.data:
+        errors['password'] = ["Password is required."]
+    if errors:
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
     user = get_object_or_404(User, username=request.data['username'])
+
     if not user.check_password(request.data['password']):
-        return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+
     token, created = Token.objects.get_or_create(user=user)
     serializer = UserSerializer(instance=user , context={'request': request})
-    return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_200_OK)
+    return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
 
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def register(request , format=None):
+    serialiazer = UserSerializer(data=request.data , context={'request': request})
+
+    if serialiazer.is_valid():
+
+        if len(serialiazer.validated_data['password']) < 8:
+            return  Response({"detail": "password too short"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = serialiazer.save()
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key, 'user': serialiazer.data}, status=status.HTTP_201_CREATED)
+
+        except IntegrityError:
+            return Response({"detail": "Username or email already exists."}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serialiazer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileViewSet(viewsets.ViewSet):
     authentication_classes = [TokenAuthentication]
